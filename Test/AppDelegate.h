04adc0be7abd352cce09fe68be846bcfb981e358
mohit-kumar <mkumar@westagilelabs.com>
2014-03-04 09:57:18 +0530

    commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by SURESH on 03/03/14.
+//  Copyright (c) 2014 VVDN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by SURESH on 03/03/14.
+//  Copyright (c) 2014 VVDN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
